91c00c325af0cd63e5f658d2080efc9a600278ed
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-01-26 13:33:59 -0700

    Initial Commit, React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import CardList from './components/cardlist/cardlist.component';
+import SearchBar from './components/searchbar/searchbar.component';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  
+  useEffect(() => {
+    const fetchMonsters = async () => {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setMonsters(response.data);
+    };
+
+    fetchMonsters();
+  }, []);
+
+  useEffect(() => {
+    let filtered = [];
+    if (searchInput === "") {
+      filtered = monsters;
+    } else {
+      filtered = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchInput.toLowerCase())
+      );
+    }
+    setFilteredMonsters(filtered);
+  })
+  
+  const handleInput = e => {
+    setSearchInput(e.target.value)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>MonstoDB</h1>
+      <SearchBar placeholder='Search Monster' handleInput={handleInput} />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
