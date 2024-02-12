a5f877cf98ec1ca60aa67f33138d1fb54b4dd0ed
nealhamacher <134631360+nealhamacher@users.noreply.github.com>
2024-02-11 22:20:01 -0700

    Finished and tested delete on backend

--- backend/repositories/monster.repository.js
+++ backend/repositories/monster.repository.js
@@ -37,7 +37,7 @@ const updateMonsterRepo = async (query, update) => {
 
 const deleteMonsterRepo = async (query) => {
     try {
-        const monster = await Monster.findOneAndDelee({ ...query });
+        const monster = await Monster.findOneAndDelete({ ...query });
         return monster;
     } catch (error) {
         throw Error ("Unable to delete")
